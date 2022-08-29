e5b65b56b4dc42077b72a80ceff68c6ef217663b
miunmnn <esteban.principe@utec.edu.pe>
2022-08-28 20:20:25 -0500

    get Users

--- hackathon-1/lib/db.js
+++ hackathon-1/lib/db.js
@@ -1,7 +1,4 @@
-// db.js
 import { Pool } from "pg";
-
-// const sql = postgres({ /* options */ }) // will use psql environment variables
 
 let conn;
 
@@ -9,17 +6,10 @@ if(!conn){
   conn = new Pool({
     user: "user",
     password: "pass",
-    host: "database_postgres_image",
-    port: "5431",
+    host: "database",
+    port: "5432",
     database: "postgres"
   })
 }
 
 export default conn
-
-// // db.js
-// import postgres from 'postgres'
-
-// const sql = postgres({ /* options */ }) // will use psql environment variables
-
-// export default sql
