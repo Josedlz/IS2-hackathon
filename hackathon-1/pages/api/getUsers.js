6de6acb49dbfa5a4a38df2646a6111564af3abca
miunmnn <esteban.principe@utec.edu.pe>
2022-09-02 20:01:57 -0500

    update login validation

--- hackathon-1/pages/api/getUsers.js
+++ hackathon-1/pages/api/getUsers.js
@@ -3,7 +3,6 @@ import conn from "../../lib/db.js";
 export default async(req, res) =>{
 
   try{
-    console.log('getting users');
     const query = `SELECT * FROM users`;
     const result = await conn.query(query)  
     res.status(200).json(result.rows); 
